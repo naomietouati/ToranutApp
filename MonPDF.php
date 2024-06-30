9755b6b37e4ef9d3ec2d9dfa422d98e483e11bf0
naomietouati <naomietouati22@gmail.com>
2024-06-30 18:16:09 +0200

    new design

--- MonPDF.php
+++ MonPDF.php
@@ -15,7 +15,7 @@ function genererPDF($tableauxTaches)
         $this->AddPage('P', 'A4');
 
       
-        $this->Image('./images/fondpdf.jpg', 0, 0, $this->GetPageWidth(), $this->GetPageHeight(), '', '', '', false);
+        $this->Image('./images/table.jpg', 0, 0, $this->GetPageWidth(), $this->GetPageHeight(), '', '', '', false);
 
  
         $this->SetFont('Arial', '', 12);
